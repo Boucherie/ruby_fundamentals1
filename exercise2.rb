d8f721dd527c75d4d8aabc189bc7a442a406507d
Emily Boucher <emi.boucher@gmail.com>
2017-05-15 13:22:23 -0400

    ex 2 no 4 boolean expression, plus comments

--- exercise2.rb
+++ exercise2.rb
@@ -1,5 +1,10 @@
+# converted answer to string to get second answer
 puts "$55 plus 20% tip"
 puts (55 * 1.20).to_s + " total cost with tip"
 
-
+#interpolated product
 puts "The product of 45628 by 7839 is #{45628 * 7839}."
+
+#Should come out as true - first is false, second is true
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
