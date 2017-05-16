8b3a4145b6fd436cc974b9d74997410c599095d8
Emily Boucher <emi.boucher@gmail.com>
2017-05-15 20:14:50 -0400

    COmpleted string interpolation as subtotal calculation, redid drink orders to do the same, rewrote dice roller for coupon, added a timer on a while loop.

--- exercise5.rb
+++ exercise5.rb
@@ -1,27 +1,27 @@
-puts "How many pizzas do you want to order?"
+puts "How many pizzas do you want to order, for $14 each?"
 quantity = gets.chomp
 
-order_size = "You are ordering #{quantity.to_i} pizzas"
+puts "You are ordering #{quantity.to_i} pizzas."
 
+puts "Your subtotal for the pizza comes to $#{quantity.to_i * 14}. Would you like to add drinks for $2 each? Please enter the number of drinks."
 
-"How many drinks would you like to order with that? They're on special today."
 quantity = gets.chomp
+puts "You have selected #{quantity.to_i} drinks for $#{quantity.to_i * 2}."
 
-drink_order = "#{quantity.to_i} drinks to go."
+puts "We have a Prime special on for today!  Roll the die to get a coupon for one of our specialty pizzas, while you wait 10 minutes for your pizza to be ready."
 
-puts "We have a Prime special on for today!  Pick a number between one to one hundred to get one of our specialty pizzas at a discount."
-
-options = gets.chomp
-
-options = (1..100)
-options do |num|
-  if !(num % 2 == 0) && !(num % 3 == 0) && !(num % 5 == 0) && !(num % 7 == 0)
-    puts "Deluxe"
-  elsif (num % 3 == 0) && !(num % 5 == 0) && !(num % 7 == 0)
+discount = 1 + rand(6)
+  if discount <= 2
     puts "Hawaiian"
-  elsif !(num % 5 == 0) && !(num % 7 == 0)
-    puts "Pepperoni"
+  elsif discount > 4
+    puts "Deluxe"
   else
-    puts "Cheese"
+    puts "Veggie"
   end
-end
+
+timer = 10
+  while timer > 0
+    puts "#{timer} minutes left before your order is ready."
+    timer -= 1
+  end
+puts "Come back again soon!"
