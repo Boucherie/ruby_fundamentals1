6d538ab7701406d2af65b715cc64a34841a3843f
Emily Boucher <emi.boucher@gmail.com>
2017-05-16 11:06:40 -0400

    exercises 1 to 5, exercise 6 started...

--- exercise6.rb
+++ exercise6.rb
@@ -8,10 +8,14 @@ while total < 100
   if trek == "walk"
     total = total + 1
     puts "Distance from home is #{total} km."
-  return
+    puts "Would you like to walk, or run?"
+    trek = gets.chomp
+    return
   elsif trek == "run"
     total = total + 5
     puts "Distance from home is #{total} km"
+    puts "Would you like to walk, or run?"
+    trek = gets.chomp
   return
   else
     "Please try again."
