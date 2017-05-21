88d35dad9ed314064082d82271dff5037138e5e8
Emily Boucher <emi.boucher@gmail.com>
2017-05-21 16:46:37 -0400

    Got initial walk/run look working

--- exercise6.rb
+++ exercise6.rb
@@ -1,23 +1,23 @@
 total = 0
 
 
-puts "Would you like to walk, or run?"
+puts "Walk, or run?"
 
 trek = gets.chomp
-while total < 100
+while true
   if trek == "walk"
-    total = total + 1
+    total += 1
     puts "Distance from home is #{total} km."
-    puts "Would you like to walk, or run?"
-    trek = gets.chomp
-    return
+
   elsif trek == "run"
-    total = total + 5
+    total += 5
     puts "Distance from home is #{total} km"
-    puts "Would you like to walk, or run?"
-    trek = gets.chomp
-  return
+
   else
     "Please try again."
   end
+
+puts "Walk, or run?"
+trek = gets.chomp
+
 end
