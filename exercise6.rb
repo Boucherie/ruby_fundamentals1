9e7c0f223a732046d2f1028f07a45756a4ee2801
Emily Boucher <emi.boucher@gmail.com>
2017-05-21 17:20:03 -0400

    Added energy levels to exercise 6, options to rest and go home; if statement for run if energy <=0

--- exercise6.rb
+++ exercise6.rb
@@ -1,23 +1,38 @@
 total = 0
+energy = 20
 
 
-puts "Walk, or run?"
+puts "Walk, run, rest or go home?"
 
 trek = gets.chomp
 while true
+
   if trek == "walk"
     total += 1
-    puts "Distance from home is #{total} km."
-
+    energy += 2
+    puts "Distance from home is #{total} km. Your energy level is #{energy} units."
   elsif trek == "run"
+    if energy <= 0 #this lets you go once past 0, then catches itself...bug
+      energy = 0
+      total == total
+      puts "Time to stop running! Try another option."
+    else
     total += 5
-    puts "Distance from home is #{total} km"
-
+    energy -= 4
+        puts "Distance from home is #{total} km. Your energy level is #{energy} units."
+    end
+  elsif trek == "rest"
+    energy += 8
+    puts "Time to stop and rest. Distance from home is #{total} km. Your energy level is #{energy} units."
+  elsif trek == "go home"
+    puts "Time to head home."
+    break
   else
-    "Please try again."
+    puts "Please try again."
   end
 
-puts "Walk, or run?"
+
+puts "Walk, run, rest or go home?"
 trek = gets.chomp
 
 end
